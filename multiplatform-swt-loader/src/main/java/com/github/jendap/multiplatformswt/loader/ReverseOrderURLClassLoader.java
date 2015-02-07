e64dda4bc59efb3cbf8a6f3da5aee1035eeff417
Jan Prach <jendap@gmail.com>
2015-02-07 12:40:18 -0800

    change ReverseOrderURLClassLoader to look like java 8
    BEWARE: I'm not sure if it works with older java versions.
            It should work with java 7 at least.

--- multiplatform-swt-loader/src/main/java/com/github/jendap/multiplatformswt/loader/ReverseOrderURLClassLoader.java
+++ multiplatform-swt-loader/src/main/java/com/github/jendap/multiplatformswt/loader/ReverseOrderURLClassLoader.java
@@ -3,36 +3,50 @@ package com.github.jendap.multiplatformswt.loader;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-public class ReverseOrderURLClassLoader extends URLClassLoader {
+final public class ReverseOrderURLClassLoader extends URLClassLoader {
+	static {
+		ClassLoader.registerAsParallelCapable();
+	}
+
 	public ReverseOrderURLClassLoader(final URL[] urls) {
 		super(urls);
 	}
 
-	public ReverseOrderURLClassLoader(final URL[] urls,
-			final ClassLoader parentClassLoader) {
+	public ReverseOrderURLClassLoader(final URL[] urls, final ClassLoader parentClassLoader) {
 		super(urls, parentClassLoader);
 	}
 
 	@Override
-	protected synchronized Class<?> loadClass(final String name, final boolean resolve) throws ClassNotFoundException {
-		// First, check if the class has already been loaded
-		Class<?> c = findLoadedClass(name);
-		if (c == null) {
-			// invoke findClass in order to find the class.
-			try {
-				c = findClass(name);
-			} catch (ClassNotFoundException e) {
-				// ClassNotFoundException thrown if class not found
-				// from the non-null parent class loader
+	public final Class<?> loadClass(final String name, final boolean resolve) throws ClassNotFoundException {
+		synchronized (getClassLoadingLock(name)) {
+			// First, check if the class has already been loaded
+			Class<?> c = findLoadedClass(name);
+			if (c == null) {
+				// invoke findClass in order to find the class.
+				try {
+					c = findClass(name);
+				} catch (ClassNotFoundException e) {
+					// ClassNotFoundException thrown if class not found
+					// from the non-null parent class loader
+				}
 			}
+			if (c == null && this.getParent() != null) {
+				try {
+					c = this.getParent().loadClass(name);
+				} catch (ClassNotFoundException e) {
+					// ClassNotFoundException thrown if class not found
+					// from the non-null parent class loader
+				}
+			}
+			if (c == null) {
+				// If still not found, then invoke findClass in order
+				// to find the class.
+				c = findClass(name);
+			}
+			if (resolve) {
+				resolveClass(c);
+			}
+			return c;
 		}
-		if (c == null && getParent() != null) {
-			// If still not found, then try call parent
-			c = getParent().loadClass(name);
-		}
-		if (resolve) {
-			resolveClass(c);
-		}
-		return c;
 	}
 }
